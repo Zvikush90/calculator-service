aa88c825117fa335a6eb83444c3f46793af182e1
zvil <zvil@algotec.co.il>
2018-04-24 09:26:11 +0300

    adding operator tests after digit

--- test/test.js
+++ test/test.js
@@ -19,6 +19,61 @@ describe('Calculator Module', function() {
       expect(JSON.parse(s).display).to.equal("12345");
     });
   });
+
+  describe('Number Concat', function() {
+    it('should accept + after number and continue to display 12345', function() {
+      let s = null
+      s = calc.calculateNextState(s, "1") // 1
+      expect(JSON.parse(s).display).to.equal("1");
+      s = calc.calculateNextState(s, "2") // 12
+      expect(JSON.parse(s).display).to.equal("12");
+      s = calc.calculateNextState(s, "3") // 123
+      expect(JSON.parse(s).display).to.equal("123");
+      s = calc.calculateNextState(s, "4") // 1234
+      expect(JSON.parse(s).display).to.equal("1234");
+      s = calc.calculateNextState(s, "5") // 12345
+      expect(JSON.parse(s).display).to.equal("12345");
+      s = calc.calculateNextState(s, "+") // 12345
+      expect(JSON.parse(s).display).to.equal("12345");
+    });
+  });
+
+  describe('Number Concat', function() {
+    it('should accept - after number and continue to display 12345', function() {
+      let s = null
+      s = calc.calculateNextState(s, "1") // 1
+      expect(JSON.parse(s).display).to.equal("1");
+      s = calc.calculateNextState(s, "2") // 12
+      expect(JSON.parse(s).display).to.equal("12");
+      s = calc.calculateNextState(s, "3") // 123
+      expect(JSON.parse(s).display).to.equal("123");
+      s = calc.calculateNextState(s, "4") // 1234
+      expect(JSON.parse(s).display).to.equal("1234");
+      s = calc.calculateNextState(s, "5") // 12345
+      expect(JSON.parse(s).display).to.equal("12345");
+      s = calc.calculateNextState(s, "-") // 12345
+      expect(JSON.parse(s).display).to.equal("12345");
+    });
+  });
+
+  describe('Number Concat', function() {
+    it('should accept * after number and continue to display 12345', function() {
+      let s = null
+      s = calc.calculateNextState(s, "1") // 1
+      expect(JSON.parse(s).display).to.equal("1");
+      s = calc.calculateNextState(s, "2") // 12
+      expect(JSON.parse(s).display).to.equal("12");
+      s = calc.calculateNextState(s, "3") // 123
+      expect(JSON.parse(s).display).to.equal("123");
+      s = calc.calculateNextState(s, "4") // 1234
+      expect(JSON.parse(s).display).to.equal("1234");
+      s = calc.calculateNextState(s, "5") // 12345
+      expect(JSON.parse(s).display).to.equal("12345");
+      s = calc.calculateNextState(s, "*") // 12345
+      expect(JSON.parse(s).display).to.equal("12345");
+    });
+  });
+
 });
 
 // let s = null
